49960328edbcb6dd8a4de2915dd599027dbb6957
maca <narrow617@gmail.com>
2022-03-21 18:27:55 +0900

    edit view

--- lib/main.dart
+++ lib/main.dart
@@ -7,19 +7,23 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter Demo'),
-        ),
-        body: Center(
-          child: Text('Hello World'),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Icon(Icons.add),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Text('Bold',style: TextStyle(fontWeight: FontWeight.bold)),
+            Text('Italic',style: TextStyle(fontStyle: FontStyle.italic)),
+            Text('fontSize = 36',style: TextStyle(fontSize: 36)),
+            Text('Red',style: TextStyle(color: Colors.red)),
+            Text('TextAlign.right', textAlign: TextAlign.right),
+            Container(
+              width: double.infinity,
+              color: Colors.grey,
+              // 表示位置を指定
+              child: Text('TextAlign.right', textAlign: TextAlign.right),
+            ),
+          ],
         ),
       ),
     );
